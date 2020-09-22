3846616fc7d23e5d38cfaa634f3eed34ff0edc5f
yann021 <y.bonnard4@gmail.com>
2020-09-22 16:25:41 +0200

    Tst10.py

---
+++ FirstTest/Tst10.py
@@ -0,0 +1,7 @@
+ma_list = ['Jean','Maximilien','Brigitte','Sonia','Jean-Pierre','Sandra']
+minList = [i for i in ma_list if len(i>=6)]
+mmaxList = [i for i in ma_list if len(i<6)]
+
+[x for x in ma_list if len(x)>=6]
+
+
