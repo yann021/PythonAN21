974f47fce5c4a627464a1b1a137a56d9089f0bfb
yann021 <y.bonnard4@gmail.com>
2020-09-22 11:03:14 +0200

    Corection for Tst3.py

--- FirstTest/Tst3.py
+++ FirstTest/Tst3.py
@@ -1,6 +1,8 @@
 #convert KM/H to M/H
 
 miles = 1.609
-Vitesse = input("Saisir une vitesse en KM/H  -> ")
+Vitesse = input("Saisir une vitesse en KM/H \n -> ")
 rVitesse = float(Vitesse) * miles
 print (rVitesse)
+
+#vitesse_en_miles = (float(imput("Saisir une vitesse en KM/H \n -> ")))*1.609
