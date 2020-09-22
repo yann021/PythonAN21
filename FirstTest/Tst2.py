dde6ef34dd3af77f499795eb9db05946288b0645
yann021 <y.bonnard4@gmail.com>
2020-09-22 10:11:38 +0200

    Tst2.py

---
+++ FirstTest/Tst2.py
@@ -0,0 +1,12 @@
+a = 5
+b = 6
+print( "a vaut "+ str(a)) 
+print( "b vaut "+ str(b)) 
+c = a
+a = b
+b = c
+print( "a vaut maintenant "+ str(a)) 
+print( "b vaut maintenant " +str(b)) 
+
+# Or
+# a, b = b, a
